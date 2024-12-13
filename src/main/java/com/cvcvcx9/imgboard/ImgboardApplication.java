a0f0331fc5786ef3c4502fb34ed67561b15f2011
cvcvcx9 <cvcvcx9@gmail.com>
2024-12-13 17:28:59 +0900

    Init:first init

---
+++ src/main/java/com/cvcvcx9/imgboard/ImgboardApplication.java
@@ -0,0 +1,13 @@
+package com.cvcvcx9.imgboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImgboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImgboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cvcvcx9/imgboard/ImgboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cvcvcx9.imgboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImgboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
