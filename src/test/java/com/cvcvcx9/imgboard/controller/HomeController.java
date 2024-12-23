6f61acfe298f151f8edef9bfeb6d1cd55e646877
cvcvcx9 <cvcvcx9@gmail.com>
2024-12-24 00:37:20 +0900

    Test:jenkinsTest

--- src/test/java/com/cvcvcx9/imgboard/controller/HomeController.java
+++ src/test/java/com/cvcvcx9/imgboard/controller/HomeController.java
@@ -13,7 +13,7 @@ public class HomeController {
 
     @GetMapping("")
     public String hello(){
-        log.info("tes7612113113221111232211212t");
+        log.info("te");
         return "hello";
     }
 
