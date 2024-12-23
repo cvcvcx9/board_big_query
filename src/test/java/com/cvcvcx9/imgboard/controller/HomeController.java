d9581f32afce9cef49923f2382fee945bbdf90bb
cvcvcx9 <cvcvcx9@gmail.com>
2024-12-24 00:11:40 +0900

    Test:test5

--- src/test/java/com/cvcvcx9/imgboard/controller/HomeController.java
+++ src/test/java/com/cvcvcx9/imgboard/controller/HomeController.java
@@ -13,7 +13,7 @@ public class HomeController {
 
     @GetMapping("")
     public String hello(){
-        log.info("tes761211322311212t");
+        log.info("tes76121132112311212t");
         return "hello";
     }
 
