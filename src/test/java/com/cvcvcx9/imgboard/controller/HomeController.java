53430a9e65c5290093203eaa64c30f1a1b35ad47
cvcvcx9 <cvcvcx9@gmail.com>
2024-12-23 19:03:47 +0900

    Test:test

--- src/test/java/com/cvcvcx9/imgboard/controller/HomeController.java
+++ src/test/java/com/cvcvcx9/imgboard/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.cvcvcx9.imgboard.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
+    private static final Logger log = LogManager.getLogger(HomeController.class);
+
     @GetMapping("")
     public String hello(){
+        log.info("test");
         return "hello";
     }
 
