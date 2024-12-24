ae2e1fee0144bccd8b89f15cf5228a439641c40d
cvcvcx9 <cvcvcx9@gmail.com>
2024-12-24 18:28:20 +0900

    Feat:create Boart Dtos and service

--- src/main/java/com/cvcvcx9/imgboard/config/securityConfig.java
+++ src/main/java/com/cvcvcx9/imgboard/config/securityConfig.java
@@ -1,7 +1,12 @@
 package com.cvcvcx9.imgboard.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -9,7 +14,9 @@ public class securityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
+            .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(s->
+                        s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/").permitAll()
                 .anyRequest().authenticated()
